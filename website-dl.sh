f39026860a3ec4011230bb4467d8ef1c3fc75aca
Steven Wright <stevus06@gmail.com>
2019-06-14 09:19:29 -0700

    Use only the original domain; use args

--- website-dl.sh
+++ website-dl.sh
@@ -1,1 +1,2 @@
-wget -T 10 -r -e robots=off -E -H -k -K -p -nH -nd -P /tmp/path URL
+# wget-website-dl.sh /tmp/site site.com
+wget -T 10 -r -e robots=off -E -H -k -K -p -nH -nd --domains $2 -P $1 $2
